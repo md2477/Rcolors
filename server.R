4b5fc0fa4df73b5fa071f4c8fe257a4c46b46ab9
md2477 <md2477@gmail.com>
2014-01-30 22:01:10 +0000

    Combined color name and code into color box, removed HTML above and below box

--- server.R
+++ server.R
@@ -4,13 +4,17 @@ library(shiny)
 shinyServer(
     function(input, output) {
 
+        colorName <- reactive({
+            input$rColorSelected
+        })
+        
         colorCode <- reactive({
-            rgbValues <- as.raw(col2rgb(input$rColorSelected))
+            rgbValues <- as.raw(col2rgb(colorName()))
             colorCode <- sprintf("#%s%s%s", rgbValues[1], rgbValues[2], rgbValues[3])
         })
         
         output$colorBox <- renderText({
-            HTML(sprintf("<div class='colorBox'></div>", colorCode()))
+            HTML(sprintf("<div class='colorBox' style='background-color: %s;'><h2>%s</h2><h2>%s</h2></div>", colorCode(), colorName(), colorCode()))
         })
         
         output$rgbBreakdown <- renderText({
