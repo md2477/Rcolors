c14b9c91607302fe9e6590d10098231836ab4b28
md2477 <md2477@gmail.com>
2014-01-30 01:20:33 +0000

    Added explanation of what this app is for, along with contact info and links

--- ui.R
+++ ui.R
@@ -12,9 +12,31 @@ shinyUI(
             
             selectInput(
                 inputId = "rColorSelected",
-                label   = "R colors",
+                label   = "Choose a color name:",
                 choices = as.list(colors())
-                )
+                ),
+            
+            h2("What can I use this for?"),
+            
+            h5("The colors listed in the dropdown menu above can be used in the color argument
+                of any R function. For example, instead of"),
+            
+            code("plot(mtcars$wt, mtcars$mpg, col = '#5f9ea0'"),
+            
+            
+            h5("...try this:"),
+            
+            code("plot(mtcars$wt, mtcars$mpg, col = 'cadetblue'"),
+            
+            h5("Using color names make it easier to remember your color scheme, instead of 
+                always hunting through your code for the right hexadecimal value."),
+            
+            div(HTML("<br>This app was created by Mason DeCamillis.
+                     <br>Code available at <a href='https://github.com/mdec/Rcolors' target='_blank'>https://github.com/mdec/Rcolors</a>.
+                     <br>See more at <a href='http://www.showmeshiny.com' target='_blank'>showmeshiny.com</a> and follow <a href='http://www.twitter.com/showmeshiny' target='_blank'>@showmeshiny</a>."))
+            
+            
+            
             
         ),
           
